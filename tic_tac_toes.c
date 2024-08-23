ab553c94b5fe092c0ea2604330a4bc4a56420685
Γιώργος Σιγιολτζάκης <82608609+GSigioltzakis@users.noreply.github.com>
2024-08-23 15:49:28 +0300

    Update in english

--- tic_tac_toes.c
+++ tic_tac_toes.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-//arxika tha kano mia sinartisi pou tha vlepoume tin triliza (pinaka) kai tha ton alazoume
+// Initially, I will create a function that will display the tic-tac-toe board and update it.
 void printBoard(char keno[9]) {
     printf(" %c | %c | %c \n", keno[0], keno[1], keno[2]);
     printf("---+---+---\n");
@@ -9,59 +9,58 @@ void printBoard(char keno[9]) {
 }
 
 int main(){
-	int i; //gia for loops
-	char keno[9]; //o pinakas pou tha vazoume ta simvola
-	
-	for (i=0; i < 9; i++){
-			keno[i] = ' ';
-	}
+    int i; // for loops
+    char keno[9]; // the board where we will place the symbols
+    
+    for (i = 0; i < 9; i++) {
+        keno[i] = ' ';
+    }
 
-	printBoard(keno);
-	
-	char player = 'X';
-	int num; //i apadsisi tou paikti
-	int count = 11; //count ama einai tie
-	
-	while (1) {
-		printf("Player %c: ", player);
-		scanf("%d", &num); //asks the answer
-		if (num >= 1 && num <= 9 && keno[num - 1] == ' ') {
+    printBoard(keno);
+    
+    char player = 'X';
+    int num; // the player's input
+    int count = 11; // count to check if it's a tie
+    
+    while (1) {
+        printf("Player %c: ", player);
+        scanf("%d", &num); // asks for the player's input
+        if (num >= 1 && num <= 9 && keno[num - 1] == ' ') {
             keno[num - 1] = player;
             printBoard(keno);
 
-
-			for (i = 0; i < 9; i += 3) {
-                if (keno[i] == keno[i + 1] && keno[i + 1] == keno[i + 2] && keno[i] != ' ') {//orizodies
+            for (i = 0; i < 9; i += 3) {
+                if (keno[i] == keno[i + 1] && keno[i + 1] == keno[i + 2] && keno[i] != ' ') { // horizontal
                     printf("Player %c won\n", player);
                     return 0; // End the game
                 }
             }
 
             for (i = 0; i < 3; i++) {
-                if (keno[i] == keno[i + 3] && keno[i + 3] == keno[i + 6] && keno[i] != ' ') {//kathetes
+                if (keno[i] == keno[i + 3] && keno[i + 3] == keno[i + 6] && keno[i] != ' ') { // vertical
                     printf("Player %c won\n", player);
                     return 0; // End the game
                 }
             }
 
-            if ((keno[0] == keno[4] && keno[4] == keno[8] && keno[0] != ' ') || (keno[2] == keno[4] && keno[4] == keno[6] && keno[2] != ' ')) { //diagonies
+            if ((keno[0] == keno[4] && keno[4] == keno[8] && keno[0] != ' ') || (keno[2] == keno[4] && keno[4] == keno[6] && keno[2] != ' ')) { // diagonal
                 printf("Player %c won\n", player);
                 return 0; // End the game
             }
-			//switching players
-			if (player == 'X') {
-				player = '0';
-			} else {
-				player = 'X';
-			}
+            // switching players
+            if (player == 'X') {
+                player = '0';
+            } else {
+                player = 'X';
+            }
 
-		}/*else {
-			printf("\0");    AN to vgalo apo comment tha mou emfanisei warning gia to \0 :D
-		}*/
-		count -=1;
-		if (count==0) {
-			printf("Tie\n");
-			return 0;
-		}	
-	}
+        }/*else {
+            printf("\0");    If I remove the comment, it will show a warning for \0 :D
+        }*/
+        count -= 1;
+        if (count == 0) {
+            printf("Tie\n");
+            return 0;
+        }    
+    }
 }
