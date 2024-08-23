19e1611352e21c96688b73bd61d80ec1d8c2e6b4
Γιώργος Σιγιολτζάκης <82608609+GSigioltzakis@users.noreply.github.com>
2024-08-23 15:52:13 +0300

    Update tower_of_hanoi in english

--- tower_of_hanoi.c
+++ tower_of_hanoi.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int val; //global gia na m in itn xrisimopoio sixna
-
-
+int val; // global to avoid using it frequently
 
 void repeatCharacter(char character, int times) {
     for (int i = 0; i < times; i++)
@@ -11,23 +9,23 @@ void repeatCharacter(char character, int times) {
 }
 
 void printNumber(int num) {
-    int requiredDigits = num * 2 - 1; //gia to motivo
+    int requiredDigits = num * 2 - 1; // for the pattern
     char *numStr;
-    size_t numStrLen; //to xrisomopoio mono gia to numStrLen
+    size_t numStrLen; // used only for numStrLen
     if (num == 0) {
         printf("|");
     } else if (num < 10) {
         repeatCharacter(num + '0', requiredDigits);
     } else {
         numStrLen = snprintf(NULL, 0, "%d", num) + 1;
-        /*int snprintf(char *str, size_t size, const char *format, ...); apo manual
-        episis to +1 gia na prosthesei sto telos to /0 null, opos einai kathe sting*/
+        /* int snprintf(char *str, size_t size, const char *format, ...); from manual
+           also, the +1 is to add the /0 null at the end, as with every string */
 
-        numStr = malloc(numStrLen); //mem allocate diaxirisi mnimis
+        numStr = malloc(numStrLen); // memory allocation for memory management
         sprintf(numStr, "%d", num);
-        /*int sprintf(char *str, const char *format, ...); apo manual
-        to string tha apothikefti sto pointer numStr giafto kai kaname malloc
-        ama diladi einai >=10 tote apothikevete prota to 1 kai meta to 0 kai meta to /0(null) */
+        /* int sprintf(char *str, const char *format, ...); from manual
+           the string will be stored in the pointer numStr, hence we used malloc
+           if it is >=10, it stores first the 1, then the 0, and then /0 (null) */
 
         for (int i = 0; i < requiredDigits; i++) {
             printf("%c", numStr[i % (numStrLen - 1)]);
@@ -44,12 +42,12 @@ void printBoard(int board[][3]) {
     printf("B");
     repeatCharacter(' ', val * 2);
     printf("C\n\n");
-    //visualize ton pinaka(to paixnidi)
+    // visualize the board (the game)
     for (int j = 0; j < 3; j++) {
         int num = 0;
-        int widthToSatisfy = val * 2 - 1;  //total width
-        int dashSidePadding = (widthToSatisfy - 1) / 2; //pavles
-        int sidePadding = ((widthToSatisfy - 1) - ((num - 1) * 2)) / 2; //empty space ta kena
+        int widthToSatisfy = val * 2 - 1;  // total width
+        int dashSidePadding = (widthToSatisfy - 1) / 2; // dashes
+        int sidePadding = ((widthToSatisfy - 1) - ((num - 1) * 2)) / 2; // empty spaces
         if (num == 0)
             sidePadding = dashSidePadding;
 
@@ -79,23 +77,23 @@ void printBoard(int board[][3]) {
         printf("\n");
     }
 
-    int footer_length = 3 * (2 * val - 1) + 8; //length of dashes
+    int footer_length = 3 * (2 * val - 1) + 8; // length of dashes
     repeatCharacter('-', footer_length);
 }
 
 void moveToRod(int board[][3], int element, char to_rod) {
     int rowIndex, colIndex;
     for (int i = 0; i < val; i++) {
-        for (int j = 0; j < 3; j++) { // j<3 giati einai A B C stiles (columns)
+        for (int j = 0; j < 3; j++) { // j < 3 because there are A B C columns
             if (board[i][j] == element) {
-                rowIndex = i; //assigns the current row index i
-                colIndex = j; //assigns the current row index j
+                rowIndex = i; // assigns the current row index i
+                colIndex = j; // assigns the current column index j
                 break;
             }
         }
     }
 
-    int toColumnIndex = to_rod - 'A'; //afairesi apo ASCII an paradeigma to to_rod = 'C' tote C-A kanei 2(an A=0, B=1, C=3...)
+    int toColumnIndex = to_rod - 'A'; // subtract from ASCII, for example, if to_rod = 'C' then C - A is 2 (if A = 0, B = 1, C = 2...)
     int toRowIndex;
     for (int i = val - 1; i >= 0; i--) {
         if (board[i][toColumnIndex] == 0) {
@@ -107,7 +105,8 @@ void moveToRod(int board[][3], int element, char to_rod) {
     board[toRowIndex][toColumnIndex] = element;
     board[rowIndex][colIndex] = 0;
 }
-//anadromi
+
+// recursion
 void towerOfHanoi(int x, char from_rod, char to_rod, char aux_rod, int board[][3]) {
     if (x > 0) {
         towerOfHanoi(x - 1, from_rod, aux_rod, to_rod, board);
